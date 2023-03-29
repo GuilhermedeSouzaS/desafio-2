b3fcdcb0a84743ba61c178a909e8d63cf06d7343
GuilhermeSilva <114371029+GuilhermedeSouzaS@users.noreply.github.com>
2023-03-29 14:46:29 -0300

    Finalizado

--- main.js
+++ main.js
@@ -6,3 +6,8 @@ const sr = ScrollReveal({
 });
 
 ScrollReveal().reveal('.container', { delay: 200 });
+
+var typed = new Typed('.capa__texto', {
+    strings: ['O tênis Jordan é fruto de uma velha e forte parceria entre a Nike e o jogador Michael Jordan.'],
+    typeSpeed: 30,
+  });
